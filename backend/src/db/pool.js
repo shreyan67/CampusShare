2023c1227b18cf4d8473cd9af3dd5219472c0868
Shreyan <shreyanacharjee21@gmail.com>
2026-04-07 07:34:25 +0530

    fix: add ssl for render postgres

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -3,6 +3,9 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+   ssl: {
+    rejectUnauthorized: false
+  },
   max: 10,
   idleTimeoutMillis: 30000,
 })
